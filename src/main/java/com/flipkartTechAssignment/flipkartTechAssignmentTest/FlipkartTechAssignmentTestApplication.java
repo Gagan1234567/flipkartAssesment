29946648fefa4e34eaea33e242d24b44d412427d
gagan verma <vgagan971@gmail.com>
2024-02-08 04:42:36 +0530

    first commit

---
+++ src/main/java/com/flipkartTechAssignment/flipkartTechAssignmentTest/FlipkartTechAssignmentTestApplication.java
@@ -0,0 +1,13 @@
+package com.flipkartTechAssignment.flipkartTechAssignmentTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartTechAssignmentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipkartTechAssignmentTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flipkartTechAssignment/flipkartTechAssignmentTest/FlipkartTechAssignmentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flipkartTechAssignment.flipkartTechAssignmentTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartTechAssignmentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
